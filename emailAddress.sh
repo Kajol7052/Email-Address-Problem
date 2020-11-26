b64ef2ce4208436308ef08f250b267bebc66a89f
“Kajol7052” <kajol7052@gmail.com>
2020-11-26 12:56:40 +0530

    Added : Validation of Optional Special Chars Part

--- emailAddress.sh
+++ emailAddress.sh
@@ -4,7 +4,7 @@ echo "Welcome to Email Address Problem"
 
 read -p "Enter your email address : " email
 #abc.xyz@BridgeLabz.co.in
-pat="^[a-zA-Z0-9]*@[a-zA-Z0-9]+[.]+[a-z]{2,4}$"
+pat="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.]+[a-z]{2,4}$"
 
 if [[ $email =~ $pat ]]
 then
