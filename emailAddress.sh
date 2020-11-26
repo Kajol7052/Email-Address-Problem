c114727c39f4546df891c1eb429988d44c751360
“Kajol7052” <kajol7052@gmail.com>
2020-11-26 12:44:51 +0530

    Added : Validation of Starting part i.e abc

--- emailAddress.sh
+++ emailAddress.sh
@@ -1,3 +1,14 @@
 #!/bin/bash/
 
 echo "Welcome to Email Address Problem"
+
+read -p "Enter your email address : " email
+#abc.xyz@BridgeLabz.co.in
+pat="^[a-zA-Z0-9]+$"
+
+if [[ $email =~ $pat ]]
+then
+        echo "Email address $email is Valid"
+else
+        echo "Email address $mail is Invalid"
+fi
